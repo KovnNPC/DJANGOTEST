787123b36d9c7ceef3abc8a653726937d5773760
Kovn <KovnNPC@gmail.com>
2026-03-23 12:53:03 -0700

    Initial Commit

---
+++ polls/urls.py
@@ -0,0 +1,19 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # /polls/
+    path("", views.index, name="index"),
+    # /polls/#/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # /polls/#/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # /polls/#/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+    # /polls/ 
+    # /polls/ 
+    # /polls/ 
+]
+
+
